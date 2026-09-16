b84d31ec9ee41db931ef43e2f756ecee9118c5bf
Nikki DS <nikkidavsan@gmail.com>
2026-09-16 19:54:50 +1000

    Create the get_url function with try/except block and detailed comments

--- api.py
+++ api.py
@@ -2,7 +2,10 @@ import requests
 
 def get_url(url: str) -> (int, str):
     """
-    Function that will call a provided GET API endpoint url and return its status code and either its content or error message as a string
+    Function that will call a provided GET API endpoint url and return its status code and either its content or error message as a string.
+    Programmed to catch exceptions in a try/except block so callers of the functions in the other modules do not need to handle exceptions downstream.
+
+    Possible exceptions (non-exhaustive): No internet connection, DNS failure, timeout, etc.
 
     Parameters
     ----------
@@ -17,3 +20,11 @@ def get_url(url: str) -> (int, str):
         Text from API call response
     """
 
+    try:
+        # timeout set to 10 seconds to avoid hanging indefinitely if the API endpoint is unresponsive
+        response = requests.get(url, timeout=10)
+        # satisfies tuple: integer and string
+        return response.status_code, response.text
+    except requests.exceptions.RequestException as error:
+        # satisfies values even though API did not return a response. None instead of an integer for the status code to make errors distinct.
+        return None, str(error)
